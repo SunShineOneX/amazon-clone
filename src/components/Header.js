536307b64d21d2ac02c1dc7c29d0f6487e376554
Jeff Rock <SunShineOneX@gmail.com>
2020-07-31 13:24:14 -0400

    searchbar upd

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import "./Header.css"
+import { Link } from "react-router-dom";
 function Header() {
     return (
         <nav className="header">
             {/* logo on the left -> img */}
-            <img className="header_logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+            <Link to="/">
+            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+            </Link>
+
+            <input type="text" className="header__searchInput" />
+
             {/* search box */}
             {/* 3 links */}
             {/* basket icon with number */}
