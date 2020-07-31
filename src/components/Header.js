02ebaaf17d01e05447c2d72511c5037eabfabc4b
Jeff Rock <SunShineOneX@gmail.com>
2020-07-31 13:15:04 -0400

    first commit boi

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <nav className="header">
+            {/* logo on the left -> img */}
+            <img className="header_logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+            {/* search box */}
+            {/* 3 links */}
+            {/* basket icon with number */}
+        </nav>
+    )
+}
+
+export default Header;
