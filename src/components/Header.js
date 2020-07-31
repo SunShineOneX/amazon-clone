c11eaebaa469c767cd657c9b454b490c44285f4e
Jeff Rock <SunShineOneX@gmail.com>
2020-07-31 13:35:41 -0400

    header searchbar updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Header.css"
 import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
 function Header() {
     return (
         <nav className="header">
@@ -9,9 +10,13 @@ function Header() {
             <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
             </Link>
 
-            <input type="text" className="header__searchInput" />
-
             {/* search box */}
+            <div className="header__search">
+            <input type="text" className="header__searchInput" />
+            <SearchIcon className="header__searchIcon" />
+            </div>
+
+            
             {/* 3 links */}
             {/* basket icon with number */}
         </nav>
